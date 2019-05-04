46133e3ad094c2e5c873c98834d7fde70c769ce0
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2019-05-04 15:57:59 -0500

    Refactor using a Layout for small components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Layout from '../components/layout';
 
 const IndexPage = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>HELLO!</h1>
       <h2>I'm Jorge Moreno. I'm fullstack developer</h2>
       <p>
         Need a developer? <Link to="/contact">Contact me.</Link>
       </p>
-      <Footer />
-    </div>
+    </Layout>
   );
 };
 
