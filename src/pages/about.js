7a229acc704360077be5a523ac11b70141f613ff
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2019-05-04 14:23:09 -0500

    Added some pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I currently working on MIES</p>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react';
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>HELLO!</h1>
+      <h2>I'm Jorge Moreno. I'm fullstack developer</h2>
+    </div>
+  );
+};
+
+export default IndexPage;
