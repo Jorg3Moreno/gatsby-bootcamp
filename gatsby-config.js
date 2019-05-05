2ae08deec4a557615b24370404b9ba8fe4bd94f8
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2019-05-05 14:48:18 -0500

    added plugins to read markdown files and show up on blog.js page

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
     title: 'Wirepunk',
     author: 'Jorge Moreno',
   },
-  plugins: ['gatsby-plugin-sass'],
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      },
+    },
+    'gatsby-transformer-remark',
+  ],
 };
